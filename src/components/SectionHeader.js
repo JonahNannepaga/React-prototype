b1dc4bf0bdc221cf842865aecab93d3324def744
Jonah Nannepaga <jonahnannepaga@Jonahs-MacBook-Pro.local>
2020-04-12 19:13:35 +0530

    Created GlobalHeading using props.

---
+++ src/components/SectionHeader.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+/*Using ES6 syntax Arrow function  (Named Exports)*/
+
+export const Heading = (props) => (
+  <h2 className="first-comp heading">{props.title}</h2>
+);
