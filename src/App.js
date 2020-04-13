735b51ccef88ad40894d5243d6663b4dbed53d2d
Jonah Nannepaga <jonahnannepaga@Jonahs-MacBook-Pro.local>
2020-04-13 22:46:07 +0530

    Timer component.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 // import FirstComponent from "./components/FirstComponent"; /*(default export)*/
-import { FunctionalComponent } from "./components/FirstComponent"; /*(named export)*/
+// import { FunctionalComponent } from "./components/FirstComponent"; /*(named export)*/
 import ClassComponent from "./components/ClassComponent";
 import { Heading } from "./components/SectionHeader";
 import LikeStateComp from "./components/LikeStateComp";
-import CounterStateComp from "./components/CounterStateComp";
+// import CounterStateComp from "./components/CounterStateComp";
+import Timer from "./components/Timer";
 
 function App() {
   return (
@@ -16,25 +17,25 @@ function App() {
           <img src={logo} className="App-logo" alt="logo" />
           <h1>MyReactApp</h1>
         </div>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a href="/" className="App-link">
+          Home
+        </a>
+        <a href="/" className="App-link">
+          Products
+        </a>
+        <a href="/" className="App-link">
+          Contact Us
         </a>
       </header>
       <main>
-        <FunctionalComponent></FunctionalComponent>
-        <ClassComponent id="first"></ClassComponent>
-        <Heading title="Heading 1 Using props"></Heading>
+        <Heading title="Homepage"></Heading>
         <Heading title="Heading 2 Using props"></Heading>
         <ClassComponent id="second"></ClassComponent>
         <Heading title="Footer section"></Heading>
       </main>
       <footer className="App-footer">
-        <CounterStateComp></CounterStateComp>
+        {/* <CounterStateComp></CounterStateComp> */}
+        <Timer></Timer>
         <LikeStateComp></LikeStateComp>
       </footer>
     </div>
