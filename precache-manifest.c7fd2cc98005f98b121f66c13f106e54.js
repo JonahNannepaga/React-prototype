b443789666b8730fdf5ba793875e9470d7a36731
Jonah Satya Anand N <satya.anand@tadigital.com>
2020-04-20 13:16:21 +0530

    Updates

---
+++ precache-manifest.c7fd2cc98005f98b121f66c13f106e54.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfbdf0a5d73092ac5f0e5ba3dd6dbdbe",
+    "url": "/React-prototype/index.html"
+  },
+  {
+    "revision": "be1e456a5e54fc0334c8",
+    "url": "/React-prototype/static/css/main.4bd03cef.chunk.css"
+  },
+  {
+    "revision": "c30ddb4295241ff6bb94",
+    "url": "/React-prototype/static/js/2.75f199e4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-prototype/static/js/2.75f199e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be1e456a5e54fc0334c8",
+    "url": "/React-prototype/static/js/main.9422f1a5.chunk.js"
+  },
+  {
+    "revision": "def738661028728ce33f",
+    "url": "/React-prototype/static/js/runtime-main.d0580808.js"
+  },
+  {
+    "revision": "b9172317d5e2395f6eb5df0834df4e8f",
+    "url": "/React-prototype/static/media/adv_1.b9172317.png"
+  },
+  {
+    "revision": "9ba61ba213e7529fc00da5263f407d17",
+    "url": "/React-prototype/static/media/adv_3.9ba61ba2.png"
+  },
+  {
+    "revision": "1187f4a81ce89867003768166ecb1b66",
+    "url": "/React-prototype/static/media/banner_background.1187f4a8.jpg"
+  },
+  {
+    "revision": "48a3cef91420d1be7dec4c59dedcb1b4",
+    "url": "/React-prototype/static/media/blog_5.48a3cef9.jpg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/React-prototype/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "3300d3949c0ddcc656fcd8192591e532",
+    "url": "/React-prototype/static/media/new_single.3300d394.png"
+  }
+]);
